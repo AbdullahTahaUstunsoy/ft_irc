506a7028261eee6fe198c72de5b2924bc77f6936
Abdullah Taha Ustunsoy <abdullahustunsoy1@gmail.com>
2026-07-15 19:18:38 +0300

    hazırlık aşamasını genel olarak yaptım. kodu düzenleyeceğim, eksik kalan yerler hatalı yerler varsa düzelteceğim ardından event loop a geçeceğim

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include <cstring> //memset için, iostream içinde var ama garanti değil her derleyicinin kabul edeceği
 #include <sys/socket.h> //SO_REUSEADDR, SOL_SOCKET, AF_INET, SOCK_STREAM gibi sabitler de bu header'da
 #include <netinet/in.h>   // struct sockaddr_in, struct in_addr, INADDR_ANY
+#include <unistd.h> //close için
+#include <fcntl.h>  //fcntl(), F_SETFL, O_NONBLOCK için
+#include <vector>
+#include <poll.h> //poll() için
 #include "Server.hpp"
 
 int main(int argc, char **argv)
@@ -14,10 +18,43 @@ int main(int argc, char **argv)
     //argümanların doğruluğunu kontrol edeceğim.
     int opt = 1;
     Server server;
-    server.setServerFd(socket(AF_INET, SOCK_STREAM, 0)); //dinleme soketini kuruyorum. //serverFd değişkeni oluşturabilirim.
-    setsockopt(server.getServerFd(), SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)); //sunucuyu kapatığ açtığımızda portun hala kullanımda olmasını önlemek için gerekiyor.
+    server.setServerFd(socket(AF_INET, SOCK_STREAM, 0)); //fd olusturulamayabilir diye kontrol yapmalı mıyım ? dinleme soketini kuruyorum. //serverFd değişkeni oluşturabilirim.
+    setsockopt(server.getServerFd(), SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)); //int dönüyor bunu kontrol etmeli miyim ? //sunucuyu kapatığ açtığımızda portun hala kullanımda olmasını önlemek için gerekiyor.
 
     struct sockaddr_in addr;
     std::memset(&addr, 0, sizeof(addr)); //addr değişkenini sıfırlıyorum. üstüne yazacağız ama struct'ın son değişkeni sıfırlanmıyor onu da sıfırlamak için memset kullandım. 
 
-}   
+    addr.sin_family = AF_INET; //IPv4
+    addr.sin_addr.s_addr = INADDR_ANY; //tüm IP adreslerinden gelen bağlantıları kabul et.
+    addr.sin_port = htons(std::atoi(argv[1]));
+
+    if(bind(server.getServerFd(), (struct sockaddr*)&addr, sizeof(addr)) < 0)
+    {
+        std::cerr << "Error binding socket" << std::endl;
+        close(server.getServerFd());   // ← açtığın fd'yi kapat
+        return (1); //exception fırlatabilirim.
+    }
+
+    if(listen(server.getServerFd(), SOMAXCONN) < 0)
+    {
+        std::cerr << "Error listening on socket" << std::endl;
+        close(server.getServerFd());   // ← açtığın fd'yi kapat
+        return (1);
+    }
+
+    if(fcntl(server.getServerFd(), F_SETFL, O_NONBLOCK) < 0)
+    {
+        std::cerr << "Error setting socket to non-blocking" << std::endl;
+        close(server.getServerFd());
+        return (1);
+    }
+
+    std::vector<struct pollfd> pollFds;
+    struct pollfd serverPollFd;
+    serverPollFd.fd = server.getServerFd();
+    serverPollFd.events = POLLIN;
+    serverPollFd.revents = 0;
+    pollFds.push_back(serverPollFd);
+
+    poll(&pollFds[0], pollFds.size(), -1);
+}
